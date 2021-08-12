e28c1a2917ca3fbf263b907b3a50f30ab82cacf5
hydro2en <86944546+hydro2en@users.noreply.github.com>
2021-08-12 11:10:39 +0900

    Update file_upload.py

--- file_upload.py
+++ file_upload.py
@@ -1,15 +1,13 @@
 from tkinter import *
-from tkinter import filedialog
 
-root = Tk()
-root.title('filedialog study')
-
-def save():
-    global my_image # 함수에서 이미지를 기억하도록 전역변수 선언 (안하면 사진이 안보임)
-    root.filename = filedialog.asksaveasfilename(initialdir='', title='파일선택', filetypes=(
-    ('png files', '*.png'), ('jpg files', '*.jpg'), ('all files', '*.*')))
+import tkinter.filedialog
 
 
-my_btn = Button(root, text='save', command=save).pack()
- 
-root.mainloop()
+title = 'Save project as'
+
+ftypes = [('csv file', '.csv'), ('All files', '*')]
+
+filename = tkinter.filedialog.asksaveasfilename(filetypes=ftypes, title=title,
+
+                                                        initialfile='noname.csv')
+
