dbca54eea50a7e0527d8bdb91e6a8cee9b5e6d7b
hydro2en <86944546+hydro2en@users.noreply.github.com>
2021-08-12 10:09:11 +0900

    Update file_upload.py

--- file_upload.py
+++ file_upload.py
@@ -1,42 +1,15 @@
-#file_upload.py
-
 from tkinter import *
 from tkinter import filedialog
 
-def Load():
-    filename = filedialog.askopenfilename(initialdir="/", title="Select file",
-                                          filetypes=(("PPTX files", "*.pptx"),
-                                          ("all files", "*.*")))
-    print(filename)
-
-def Save():
-    filename = filedialog.asksaveasfilename(initialdir="/", title="Select file",
-                                          filetypes=(("PPTX files", "*.pptx"),
-                                          ("all files", "*.*")))
-    print(filename)
-
-def domenu():
-    print("OK")
-
 root = Tk()
-menubar = Menu(root)
-filemenu = Menu(menubar, tearoff=0)
-menubar.add_cascade(label="File", menu=filemenu)
-filemenu.add_command(label="New", command=domenu)
-filemenu.add_command(label="Open", command=Load)
-filemenu.add_command(label="Save", command=Save)
-filemenu.add_command(label="Save as...", command=Save)
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=root.quit)
-editmenu = Menu(menubar, tearoff=0)
-menubar.add_cascade(label="Edit", menu=editmenu)
-editmenu.add_command(label="Copy", command=domenu)
-editmenu.add_command(label="Paste", command=domenu)
-editmenu.add_separator()
-editmenu.add_command(label="Delete", command=domenu)
-helpmenu = Menu(menubar, tearoff=0)
-menubar.add_cascade(label="Help", menu=helpmenu)
-helpmenu.add_command(label="About...", command=domenu)
+root.title('filedialog study')
 
-root.config(menu=menubar)
+def save():
+    global my_image # 함수에서 이미지를 기억하도록 전역변수 선언 (안하면 사진이 안보임)
+    root.filename = filedialog.asksaveasfilename(initialdir='', title='파일선택', filetypes=(
+    ('png files', '*.png'), ('jpg files', '*.jpg'), ('all files', '*.*')))
+
+
+my_btn = Button(root, text='save', command=save).pack()
+ 
 root.mainloop()
